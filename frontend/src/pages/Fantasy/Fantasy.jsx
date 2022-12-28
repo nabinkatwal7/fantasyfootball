6759c301ac0084bf34ed756cdc5e613b47a10cd2
Nabin Katwal <nabinkatwal2000@gmail.com>
2022-12-28 18:17:46 +0545

    changing project stack from mern to next.

--- frontend/src/pages/Fantasy/Fantasy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Fantasy() {
-  return (
-    <div>Fantasy</div>
-  )
-}
-
-export default Fantasy
--- frontend/src/pages/LiveScore/LiveScore.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function LiveScore() {
-  return (
-    <div>LiveScore</div>
-  )
-}
-
-export default LiveScore
