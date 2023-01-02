9057bc34be6f51f180dbfc600f251f2ae79c77db
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-01-02 17:34:03 +0545

    score api creation

--- onescore/components/Navbar/Navbar.js
+++ onescore/components/Navbar/Navbar.js
@@ -1,19 +1,32 @@
 import Link from 'next/link'
 import React from 'react'
 
+import HomeIcon from '@mui/icons-material/Home';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import NfcIcon from '@mui/icons-material/Nfc';
+import Nfc from '@mui/icons-material/Nfc';
 
 function Navbar() {
   return (
     <div className='navbar' >
         <ul>
             <li>
-                <Link href='/' >Home</Link>
+                <Link href='/' >
+                    <HomeIcon/>
+                    Home
+                </Link>
             </li>
             <li>
-                <Link href='/livescore' >Live Score</Link>
+                <Link href='/livescore' >
+                    <SportsSoccerIcon />
+                    Live Score
+                </Link>
             </li>
             <li>
-                <Link href='/fantasy' >Fantasy</Link>
+                <Link href='/fantasy' >
+                    <NfcIcon />
+                    Fantasy
+                </Link>
             </li>
         </ul>
     </div>
--- onescore/pages/livescore/index.js
+++ onescore/pages/livescore/index.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+
 
 function index() {
+
+  const [scores, setScores] = useState([]);
+  useEffect(()=>{
+    async function fetchScores(){
+      const response = await fetch('http://api.football-data.org/v4/matches',{
+        headers:{
+          'X-Auth-Token':'1e33c4d0fa0a4345a5a1182e6ca9ac5a',
+          'Access-Control-Allow-Origin':'*'
+        }
+      })
+      const data = await response.json()
+      setScores(data.matches)
+      console.log(data)
+    }
+    fetchScores()
+  },[])
   return (
-    <div>Livescore</div>
+    <div>
+      {scores.map((score)=>{
+        <div key={score.id}>
+          <h2>{score.homeTeam.name}</h2>
+          <p>{score.score.fullTime.homeTeam}</p>
+          <h2>{score.awayTeam.name}</h2>
+          <p>{score.score.fullTime.awayTeam}</p>
+        </div>
+      })}
+    </div>
   )
 }
 
