da74e9ba514c50e5dd4d1459da719edd5b83baf0
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-01-07 11:41:04 +0545

    api config

--- onescore/pages/livescore/index.js
+++ onescore/pages/livescore/index.js
@@ -6,29 +6,34 @@ function index() {
 
   const [scores, setScores] = useState([]);
   useEffect(()=>{
+
+    const options = {
+      method: 'GET',
+      headers:{
+        'X-RapidAPI-Key': '7fa2c59a1cmsh4203229b559d2f2p176881jsn45a4b618065b',
+		    'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com'
+      }
+    }
+
     async function fetchScores(){
-      const response = await fetch('http://api.football-data.org/v4/matches',{
-        headers:{
-          'X-Auth-Token':'1e33c4d0fa0a4345a5a1182e6ca9ac5a',
-          'Access-Control-Allow-Origin':'*'
-        }
-      })
-      const data = await response.json()
-      setScores(data.matches)
-      console.log(data)
+      const response = await fetch('https://sportscore1.p.rapidapi.com/sports/1/events/live?page=1', options)
+      const result = await response.json();
+      console.log(result)
+      setScores(result.data);
+      console.log(scores)
     }
     fetchScores()
   },[])
   return (
     <div>
-      {scores.map((score)=>{
+      {/* {scores.map((score) => (
         <div key={score.id}>
           <h2>{score.homeTeam.name}</h2>
-          <p>{score.score.fullTime.homeTeam}</p>
+          <p>{score.homeTeam.score}</p>
           <h2>{score.awayTeam.name}</h2>
-          <p>{score.score.fullTime.awayTeam}</p>
+          <p>{score.awayTeam.score}</p>
         </div>
-      })}
+      ))} */}
     </div>
   )
 }
