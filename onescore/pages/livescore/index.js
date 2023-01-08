b53a3173f8a6f2bac39a5ca0d0c2dc0840becac6
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-01-08 19:16:33 +0545

    sidebar

--- onescore/pages/livescore/index.js
+++ onescore/pages/livescore/index.js
@@ -1,39 +1,51 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
+import Link from 'next/link'
 
 
 function index() {
 
-  const [scores, setScores] = useState([]);
-  useEffect(()=>{
+  // const [scores, setScores] = useState([]);
+  // useEffect(()=>{
 
-    const options = {
-      method: 'GET',
-      headers:{
-        'X-RapidAPI-Key': '7fa2c59a1cmsh4203229b559d2f2p176881jsn45a4b618065b',
-		    'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com'
-      }
-    }
+  //   const options = {
+  //     method: 'GET',
+  //     headers:{
+  //       'X-RapidAPI-Key': '7fa2c59a1cmsh4203229b559d2f2p176881jsn45a4b618065b',
+	// 	    'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com'
+  //     }
+  //   }
 
-    async function fetchScores(){
-      const response = await fetch('https://sportscore1.p.rapidapi.com/sports/1/events/live?page=1', options)
-      const result = await response.json();
-      console.log(result)
-      await setScores(result.data);
-      console.log(scores)
-    }
-    fetchScores()
-  },[])
+  //   async function fetchScores(){
+  //     const response = await fetch('https://sportscore1.p.rapidapi.com/sports/1/events/live?page=1', options)
+  //     const result = await response.json();
+  //     console.log(result)
+  //     await setScores(result.data);
+  //     console.log(scores)
+  //   }
+  //   fetchScores()
+  // },[])
   return (
     <div>
-      {/* {scores.map((score) => (
-        <div key={score.id}>
-          <h2>{score.homeTeam.name}</h2>
-          <p>{score.homeTeam.score}</p>
-          <h2>{score.awayTeam.name}</h2>
-          <p>{score.awayTeam.score}</p>
+      <div className='sidebar'>
+        <div  className='league-list' >
+        <h2>Top Leagues</h2>
         </div>
-      ))} */}
+        <ul className='top-leagues'>
+          <li>
+            <Link href='#' >Premier League</Link>
+          </li>
+          <li>
+            <Link href='#' >Champions league</Link>
+          </li>
+          <li>
+            <Link href='#' >LaLiga</Link>
+          </li>
+          <li>
+            <Link href='#' >French League</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
