adf9b59ade72285de8d58e78e74d6c76e784692f
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-01-15 21:16:25 +0545

    standings page. error arised

--- onescore/components/Homepage/Homepage.js
+++ onescore/components/Homepage/Homepage.js
@@ -1,15 +1,73 @@
-import React from 'react'
-
 function Homepage() {
+
+  const leagueIds = [2021, 2014, 2015, 2002, 2016];
+
+  const standing = async () => {
+  
+    const requests = leagueIds.map(id => 
+      fetch(`https://api-football-v1.p.rapidapi.com/v2/leagueTable/${id}`, {
+        headers: {
+          'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
+          'x-rapidapi-key': '7fa2c59a1cmsh4203229b559d2f2p176881jsn45a4b618065b'
+        }
+      })
+    )
+    const res = await Promise.all(requests)
+    const json = await res.map(response => response.json())
+    return { standings: json.map(data => data.api.standings) }
+
+  // const requests = async leagueIds.map(id => 
+  //   fetch(`https://api-football-v1.p.rapidapi.com/v2/leagueTable/${id}`, {
+  //     headers: {
+  //       'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
+  //       'x-rapidapi-key': '7fa2c59a1cmsh4203229b559d2f2p176881jsn45a4b618065b'
+  //     }
+  //   })
+  // )
+  // const res = await Promise.all(requests)
+  // const json = await res.map(response => response.json())
+  // return { standings: json.map(data => data.api.standings) }
+
   return (
-    <div className='home' >
-        <div className='scores-around' >
-            <h1>Scores around the world</h1>
-        </div>
-        <div>
-            <h1>News</h1>
-        </div>
-    </div>
+    <div className="standings-container">
+    {props.standings.map((standing, index) => (
+      <div key={index}>
+        <h2 className="league-name">{standing.league.name}</h2>
+        <table className="table">
+          <thead>
+            <tr className="table-head">
+              <th>Position</th>
+              <th className="team-name">Team Name</th>
+              <th>Played</th>
+              <th>Won</th>
+              <th>Drawn</th>
+              <th>Lost</th>
+              <th>GF</th>
+              <th>GA</th>
+              <th>GD</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {standing.table.map((team, index) => (
+              <tr key={index}>
+                <td className="team-position">{team.position}</td>
+                <td className="team-name">{team.team.name}</td>
+                <td>{team.playedGames}</td>
+                <td>{team.won}</td>
+                <td>{team.draw}</td>
+                <td>{team.lost}</td>
+                <td>{team.goalsFor}</td>
+                <td>{team.goalsAgainst}</td>
+                <td>{team.goalDifference}</td>
+                <td>{team.points}</td>
+              </tr>
+              ))}
+              </tbody>
+            </table>
+          </div>
+        ))}
+      </div>
   )
 }
 
--- onescore/pages/_app.js
+++ onescore/pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout/Layout'
 import '../styles/globals.css'
 import '../styles/FantasyHome.css'
+import '../components/Homepage/Homepage.css'
 
 export default function App({ Component, pageProps }) {
   return (
