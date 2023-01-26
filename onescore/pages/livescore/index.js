949494e53810921ab3a387af02ae5065b35157b3
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-01-26 16:39:01 +0545

    rendered livescores, css needed

--- onescore/pages/livescore/index.js
+++ onescore/pages/livescore/index.js
@@ -1,26 +1,45 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
-import Link from 'next/link'
+import React from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
+import Response from "../../public/response.json";
 
 function Index() {
+  // const [livescores, setLivescores] = useState([]);
 
-  const [livescores, setLivescores] = useState([]);
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/livescore')
+  //     .then(res => res.json())
+  //     .then(data => setLivescores(data))
+  //     .catch(err => console.log(err));
+  // }, []);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/livescore')
-      .then(res => res.json())
-      .then(data => setLivescores(data))
-      .catch(err => console.log(err));
-  }, []);
+  const livescores = Response;
 
   return (
-    <div>
-      {livescores.map(livescore => (
-        console.log(livescore)
+    <div className='livescore'>
+      {livescores.map((livescore) => (
+        <div key={livescore.id}>
+          <p>Matchday: {livescore.season.currentMatchday}</p>
+          <p>UTC Date: {livescore.utcDate}</p>
+          <p>Status: {livescore.status}</p>
+          <p>Matchday: {livescore.matchday}</p>
+          <p>Stage: {livescore.stage}</p>
+          <p>Group: {livescore.group}</p>
+          <p>
+            Score: {livescore.score.fullTime.homeTeam} -{" "}
+            {livescore.score.fullTime.awayTeam}
+          </p>
+          <p>Home Team: {livescore.homeTeam.name}</p>
+          <p>Away Team: {livescore.awayTeam.name}</p>
+          <p>
+            Referees:{" "}
+            {livescore.referees.map((referee) => referee.name).join(", ")}
+          </p>
+        </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default Index
+export default Index;
