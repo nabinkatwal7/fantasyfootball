7f7a0c304e95dc31f95dba3ef92fa58fab2641e9
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-01-26 19:36:37 +0545

    UI for livescore complete

--- onescore/pages/livescore/index.js
+++ onescore/pages/livescore/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import Image from "next/image"
+import Image from "next/image";
 
 import Response from "../../public/response.json";
 
@@ -9,35 +9,62 @@ function Index() {
   const [livescores, setLivescores] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/livescore')
-      .then(res => res.json())
-      .then(data => setLivescores(data))
-      .catch(err => console.log(err));
+    fetch("http://localhost:5000/livescore")
+      .then((res) => res.json())
+      .then((data) => setLivescores(data))
+      .catch((err) => console.log(err));
   }, []);
-  
+
   return (
-    <div className='livescore'>
+    <div className="livescore">
       {livescores.map((livescore) => (
-        <div key={livescore.index} className='livescorecard'>
-          <img className='country-flag' src={livescore.area.flag}  alt='country flag' />
-          <p className='country' >{livescore.area.name}</p>
-          <img className='league-logo' src={livescore.competition.emblem}  alt='league-logo' />
-          <p className='league'>{livescore.competition.name}</p>
-          <p className='current-matchday'>Matchday: {livescore.season.currentMatchday}</p>
-          <img clasName='home-team-img' src={livescore.homeTeam.crest} alt='home team' />
-          <p className='home-team'>{livescore.homeTeam.name}</p>
-          <img clasName='away-team-img' src={livescore.awayTeam.crest} alt='away team' />
-          <p className='away-team'>{livescore.awayTeam.name}</p>
-          <div className='score'>
-            <p className='score-home-ht'>HT {livescore.score.halfTime.home} :</p>
-            <p className='score-away-ht'>{livescore.score.halfTime.away}</p>
-            <p className='score-home-ft'>FT {livescore.score.fullTime.home} :</p>
-            <p className='score-away-ft'>{livescore.score.fullTime.away}</p>
-            <p className='winner' >W: {livescore.score.winner}</p>
-            <p className='time' >{livescore.score.duration}</p>
+        <div key={livescore.index} className="livescorecard">
+          <div className="country">
+            <img
+              className="country-flag"
+              src={livescore.area.flag}
+              alt="country flag"
+            />
+            <p className="country-name">{livescore.area.name}</p>
           </div>
-          <div className='referee' >
-            <p className='referee-name' >{livescore.referees.name}</p>
+          <div className="league">
+            <img
+              className="league-logo"
+              src={livescore.competition.emblem}
+              alt="league-logo"
+            />
+            <p className="league">{livescore.competition.name}</p>
+            <p className="current-matchday">
+              Matchday: {livescore.season.currentMatchday}
+            </p>
+          </div>
+          <div className="score">
+            <img
+              className="home-team-img"
+              src={livescore.homeTeam.crest}
+              alt="home team"
+            />
+            <p className="home-team">{livescore.homeTeam.name}</p>
+            <p className="score-home-ht">
+              HT {livescore.score.halfTime.home} :
+            </p>
+            <p className="score-home-ft">
+              FT {livescore.score.fullTime.home} :
+            </p>
+
+            <img
+              className="away-team-img"
+              src={livescore.awayTeam.crest}
+              alt="away team"
+            />
+            <p className="away-team">{livescore.awayTeam.name}</p>
+            <p className="score-away-ht">{livescore.score.halfTime.away}</p>
+            <p className="score-away-ft">{livescore.score.fullTime.away}</p>
+            <p className="winner">W: {livescore.score.winner}</p>
+            <p className="time">{livescore.score.duration}</p>
+          </div>
+          <div className="referee">
+            <p className="referee-name">{livescore.referees.name}</p>
             <p>{livescore.referees.nationality}</p>
           </div>
         </div>
