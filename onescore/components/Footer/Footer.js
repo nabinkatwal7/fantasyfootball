186c8b54f04a891acf91098c68aecfbf7e48f70e
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-01-27 20:13:21 +0545

    added data

--- onescore/components/Footer/Footer.js
+++ onescore/components/Footer/Footer.js
@@ -8,6 +8,12 @@ function Footer() {
         <Link href='https://github.com/nabinkatwal7/fantasyfootball' >
           <img src='https://img.icons8.com/ios-glyphs/512/github.png' />
         </Link>
+        <Link href='https://twitter.com/campistaaaaa' >
+          <img src='https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png' />
+        </Link>
+        <Link href='https://nabinkatwal7.github.io/portfolio/?fbclid=IwAR0eL6yqyQiOVbkD8nfVXvRC-aTP6OXNXjLjGaTkiWsSU7tCEgdpyX-f6IA' >
+          <img src='https://www.freepnglogos.com/uploads/logo-website-png/logo-website-web-logo-clip-art-clkerm-vector-clip-art-online-11.png' />
+        </Link>
     </div>
   )
 }
