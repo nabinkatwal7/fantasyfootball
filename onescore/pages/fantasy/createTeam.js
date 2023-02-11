c93a18f7dfa7ba55c4a852edf80e15965d8bfcbf
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-02-11 16:20:20 +0545

    createTeam contd

--- onescore/pages/fantasy/createTeam.js
+++ onescore/pages/fantasy/createTeam.js
@@ -4,8 +4,12 @@ import fetch from "isomorphic-unfetch";
 
 function createTeam() {
   const [players, setPlayers] = useState([]);
+  const [filteredPlayers, setFilteredPlayers] = useState([]);
   const [team, setTeam] = useState([]);
   const [budget, setBudget] = useState(1000);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(20);
   const router = useRouter();
 
   useEffect(() => {
@@ -13,21 +17,51 @@ function createTeam() {
       const res = await fetch("http://localhost:5000/createteam");
       const data = await res.json();
       setPlayers(data.players);
+      setFilteredPlayers(data.players);
     }
-
-    fetchPlayers();
   }, []);
 
+  useEffect(() => {
+    setFilteredPlayers(
+      players.filter(
+        (player) => player.now_cost >= minPrice && player.now_cost <= maxPrice
+      )
+    );
+  }, [minPrice, maxPrice]);
+
+  useEffect(() => {
+    setFilteredPlayers(
+      players.filter(
+        (player) =>
+          player.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          player.second_name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  }, [searchTerm]);
+
+  const searchPlayers = (players) => {
+    return players.filter((player) => {
+      return (
+        player.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        player.second_name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    });
+  };
+
   const addPlayerToTeam = (player) => {
-    if (budget - player.cost >= 0 && team.length < 15) {
+    if (
+      budget - player.now_cost >= 0 &&
+      team.length < 15 &&
+      team.indexOf(player) === -1
+    ) {
       setTeam([...team, player]);
-      setBudget(budget - player.cost);
+      setBudget(budget - player.now_cost);
     }
   };
 
   const removePlayerFromTeam = (player) => {
     setTeam(team.filter((p) => p.id !== player.id));
-    setBudget(budget + player.cost);
+    setBudget(budget + player.now_cost);
   };
 
   const handleConfirmTeam = async () => {
@@ -40,7 +74,7 @@ function createTeam() {
     });
 
     if (res.ok) {
-      router.push("/teams");
+      router.push("http://localhost:5000/createteam");
     }
   };
 
@@ -48,20 +82,54 @@ function createTeam() {
     <div>
       <h1>Create Team</h1>
       <h2>Available Budget: ${budget}</h2>
+      <input
+        type="text"
+        placeholder="Search Players"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <input
+        type="range"
+        min={0}
+        max={500}
+        step={0.5}
+        value={minPrice}
+        onChange={(e) => setMinPrice(e.target.value)}
+      />
+      <span>Minimum Price: ${minPrice} </span>
+      <input
+        type="range"
+        min={0}
+        max={500}
+        step={0.5}
+        value={maxPrice}
+        onChange={(e) => setMaxPrice(e.target.value)}
+      />
+      <span>Maximum Price: ${maxPrice}</span>
+      <br />
+
+      <button onClick={searchPlayers}>Search</button>
+      <br />
+
       <h2>Players:</h2>
       <ul>
-        {players.map((player) => (
-          <li key={player.id}>
-            {player.first_name} {player.second_name} - ${player.now_cost}
-            <button onClick={() => addPlayerToTeam(player)}>Add</button>
-          </li>
-        ))}
+        {filteredPlayers
+          .filter(
+            (player) =>
+              player.now_cost >= minPrice && player.now_cost <= maxPrice
+          )
+          .map((player) => (
+            <li key={player.id}>
+              {player.first_name} {player.second_name} - ${player.now_cost}
+              <button onClick={() => addPlayerToTeam(player)}>Add</button>
+            </li>
+          ))}
       </ul>
       <h2>Team:</h2>
       <ul>
         {team.map((player) => (
           <li key={player.id}>
-            {player.first_name} - ${player.now_cost}
+            {player.first_name} {player.second_name} - ${player.now_cost}
             <button onClick={() => removePlayerFromTeam(player)}>Remove</button>
           </li>
         ))}
