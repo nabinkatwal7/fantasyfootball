aab8d3c234c85ce083cebe9c197116b0194e406e
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-02-13 14:17:50 +0545

    added football pitch

--- onescore/pages/fantasy/createTeam.js
+++ onescore/pages/fantasy/createTeam.js
@@ -1,44 +1,137 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
+import SoccerLineUp from "react-soccer-lineup";
 
 function createTeam() {
+  const homeTeamColor = "050101";
+  const homeTeamNumberColor = "ffffff";
+  const homeGoalkeeperColor = "ffffff";
+  const homeGoalkeeperNumberColor = '050101';
+  const showHomeTeam = true
+  const homeTeamClickable= true
+
+  const buildHomeTeam = () => {
+    return {
+      squad: {
+        gk: {
+          number: 1,
+          name: 'De Gea',
+          color: `#${homeGoalkeeperColor}`,
+          numberColor: `#${homeGoalkeeperNumberColor}`,
+          onClick: homeTeamClickable
+            ? () => alert(`Home team - Player ${1}`)
+            : undefined,
+        },
+        df: [
+          {
+            number: 2,
+            onClick: homeTeamClickable
+              ? () => alert(`Home team - Player ${2}`)
+              : undefined,
+          },
+          {
+            number: 4,
+            onClick: homeTeamClickable
+              ? () => alert(`Home team - Player ${4}`)
+              : undefined,
+          },
+          {
+            number: 5,
+            onClick: homeTeamClickable
+              ? () => alert(`Home team - Player ${5}`)
+              : undefined,
+          },
+          {
+            number: 3,
+            onClick: homeTeamClickable
+              ? () => alert(`Home team - Player ${3}`)
+              : undefined,
+          },
+        ],
+        cm: [
+          {
+            number: 6,
+            onClick: homeTeamClickable
+              ? () => alert(`Home team - Player ${6}`)
+              : undefined,
+          },
+          {
+            number: 8,
+            onClick: homeTeamClickable
+              ? () => alert(`Home team - Player ${8}`)
+              : undefined,
+          },
+        ],
+        cam: [
+          {
+            number: 11,
+            onClick: homeTeamClickable
+              ? () => alert(`Home team - Player ${11}`)
+              : undefined,
+          },
+          {
+            number: 10,
+            onClick: homeTeamClickable
+              ? () => alert(`Home team - Player ${10}`)
+              : undefined,
+          },
+          {
+            number: 7,
+            onClick: homeTeamClickable
+              ? () => alert(`Home team - Player ${7}`)
+              : undefined,
+          },
+        ],
+        fw: [
+          {
+            number: 9,
+            onClick: homeTeamClickable
+              ? () => alert(`Home team - Player ${9}`)
+              : undefined,
+          },
+        ],
+      },
+      style: {
+        color: `#${homeTeamColor}`,
+        numberColor: `#${homeTeamNumberColor}`,
+      },
+    };
+  };
+
   return (
-    <div className="create-team-container">
-      <div>
-        <h3>Pick Team</h3>
-        <div>
-          <p>You are picking the team for gameweek 22</p>
-        </div>
-        <div>
-          <button>List View</button>
-        </div>
-        <div>
-          <p>Starters</p>
-          <table>
-            <thead>
-              <tr>
-                <th></th>
-                <th></th>
-                <th>Pos</th>
-                <th>GW</th>
-                <th>Goals</th>
-                <th>Assists</th>
-                <th>Points</th>
-              </tr>
-              <tr>
-                <td>Leicester</td>
-                <td>Ward</td>
-                <td>GKP</td>
-                <td>1</td>
-                <td>0</td>
-                <td>0</td>
-                <td>69</td>
-              </tr>
-            </thead>
-          </table>
+    <div className="createteam-container">
+      <div className="teaminfo">
+        <h1>Football FC</h1>
+        <h3>username</h3>
+        <h3>Team Value: 100.0M</h3>
+        <h3>Remaining in Bank: 0.0M</h3>
+      </div>
+      <div className="pitch container">
+        <SoccerLineUp
+          size={"big"}
+          color={"green"}
+          pattern={"squares"}
+          homeTeam={showHomeTeam?buildHomeTeam():undefined}
+          
+        />
+        <div className="bench">
+          <ol>
+            <li>
+              <h3>GK SUB</h3>
+            </li>
+            <li>
+              <h3>DEF SUB</h3>
+            </li>
+            <li>
+              <h3>MID SUB</h3>
+            </li>
+            <li>
+              <h3>ATT SUB</h3>
+            </li>
+          </ol>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default createTeam
+export default createTeam;
