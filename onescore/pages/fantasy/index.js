e1e9d7ced9851fbb0f688a08a3d23a9e5f4db249
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-02-14 15:11:14 +0545

    final glassmorphism design

--- onescore/pages/fantasy/index.js
+++ onescore/pages/fantasy/index.js
@@ -8,7 +8,6 @@ import PersonIcon from "@mui/icons-material/Person";
 import Groups3Icon from "@mui/icons-material/Groups3";
 import LogoutIcon from "@mui/icons-material/Logout";
 
-
 function index() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -106,7 +105,7 @@ function index() {
               <AddBoxIcon />
               Create Team
             </Link>
-            <Link 
+            <Link
               className="fantasy-links"
               href="/fantasy/viewTeams"
               className="nav-link"
@@ -151,57 +150,55 @@ function index() {
       </div>
 
       <div className="section-container">
-        <div className="player-container">
-          {players.map((player) => {
-            if (pid == player.id) {
-              const playerteam = teamMap(player.team);
-              const playerPosition = positionMap(player.element_type);
-              return (
-                <div key={player.id}>
-                  <img
-                    className="player-img"
-                    src={
-                      "https://resources.premierleague.com/premierleague/photos/players/110x140/p" +
-                      player.code +
-                      ".png"
-                    }
-                    alt="player image"
-                  />
-                  <div className="player-details">
-                    <div className="player-detail">
-                      <h1>
-                        {player.first_name} {player.second_name}
-                      </h1>
-                      <h1>{playerteam}</h1>
-                      <h1>{playerPosition}</h1>
-                      <h1>Goals: {player.goals_scored}</h1>
-                      <h1>xG: {player.expected_goals}</h1>
-                      <h1>Assists: {player.assists}</h1>
-                      <h1>xA: {player.expected_assists}</h1>
-                    </div>
-                    <div className="stats">
-                      <h3>Minutes Played: {player.minutes}</h3>
-                      <h3>Clean Sheets: {player.clean_sheets}</h3>
-                      <h3>Cost: ${player.now_cost / 10}M</h3>
-                      <h3>Cost Rank: {player.now_cost_rank_type}</h3>
-                      <h3>ICT Index: {player.ict_index}</h3>
-                      <h3>Transfers In: {player.transfers_in}</h3>
-                      <h3>Transfers Out: {player.transfers_out}</h3>
-                    </div>
-                    <div className="more-stats">
-                      <h3>Penalties Missed: {player.penalties_missed}</h3>
-                      <h3>Points Per Game: {player.points_per_game}</h3>
-                      <h3>Starts Per 90: {player.starts_per_90}</h3>
-                      <h3>Yellow Cards: {player.yellow_cards}</h3>
-                      <h3>Red Cards: {player.red_cards}</h3>
-                      <h3>Threats: {player.threat}</h3>
-                    </div>
+        {players.map((player) => {
+          if (pid == player.id) {
+            const playerteam = teamMap(player.team);
+            const playerPosition = positionMap(player.element_type);
+            return (
+              <div key={player.id}>
+                <img
+                  className="player-img"
+                  src={
+                    "https://resources.premierleague.com/premierleague/photos/players/110x140/p" +
+                    player.code +
+                    ".png"
+                  }
+                  alt="player image"
+                />
+                <div className="player-details">
+                  <div className="player-detail">
+                    <h1>
+                      {player.first_name} {player.second_name}
+                    </h1>
+                    <h1>{playerteam}</h1>
+                    <h1>{playerPosition}</h1>
+                    <h1>Goals: {player.goals_scored}</h1>
+                    <h1>xG: {player.expected_goals}</h1>
+                    <h1>Assists: {player.assists}</h1>
+                    <h1>xA: {player.expected_assists}</h1>
+                  </div>
+                  <div className="stats">
+                    <h3>Minutes Played: {player.minutes}</h3>
+                    <h3>Clean Sheets: {player.clean_sheets}</h3>
+                    <h3>Cost: ${player.now_cost / 10}M</h3>
+                    <h3>Cost Rank: {player.now_cost_rank_type}</h3>
+                    <h3>ICT Index: {player.ict_index}</h3>
+                    <h3>Transfers In: {player.transfers_in}</h3>
+                    <h3>Transfers Out: {player.transfers_out}</h3>
+                  </div>
+                  <div className="more-stats">
+                    <h3>Penalties Missed: {player.penalties_missed}</h3>
+                    <h3>Points Per Game: {player.points_per_game}</h3>
+                    <h3>Starts Per 90: {player.starts_per_90}</h3>
+                    <h3>Yellow Cards: {player.yellow_cards}</h3>
+                    <h3>Red Cards: {player.red_cards}</h3>
+                    <h3>Threats: {player.threat}</h3>
                   </div>
                 </div>
-              );
-            }
-          })}
-        </div>
+              </div>
+            );
+          }
+        })}
       </div>
     </div>
   );
