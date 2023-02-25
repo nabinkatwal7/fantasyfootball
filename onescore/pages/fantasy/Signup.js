bd19f4dc7c9dddbce546deec6828a061a36749bd
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-02-25 23:17:50 +0545

    signup page UI

--- onescore/pages/fantasy/Signup.js
+++ onescore/pages/fantasy/Signup.js
@@ -1,6 +1,12 @@
 import { React, useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 function Signup() {
   const [username, setUsername] = useState("");
@@ -9,8 +15,8 @@ function Signup() {
   const [repeatPassword, setRepeatPassword] = useState("");
   const [passwordStrength, setPasswordStrength] = useState(0);
 
-  const [status, setStatus] = useState()
-  const router = useRouter()
+  const [status, setStatus] = useState();
+  const router = useRouter();
 
   const [formData, setFormData] = useState({
     username: "",
@@ -19,20 +25,20 @@ function Signup() {
     repeatPassword: "",
   });
 
-  useEffect(()=>{
-    if(status === 'success'){
-      router.push('/fantasy/Login')
+  useEffect(() => {
+    if (status === "success") {
+      router.push("/fantasy/Login");
     }
-  },[status])
+  }, [status]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/register', {
-        method: 'POST',
-        headers: { 
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': "*" 
+      const response = await fetch("http://localhost:5000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
         },
         body: JSON.stringify(formData),
       });
@@ -40,12 +46,12 @@ function Signup() {
       if (data.error) {
         alert(data.error);
       } else {
-        alert('Successfully registered');
+        alert("Successfully registered");
       }
-      if (response.ok){
-        setStatus('success')
-      }else{
-        setStatus('error')
+      if (response.ok) {
+        setStatus("success");
+      } else {
+        setStatus("error");
       }
     } catch (err) {
       console.error(err);
@@ -60,58 +66,78 @@ function Signup() {
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-  
+
   return (
     <div className="signupdiv">
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <h2 className="form-title">Start your journey today!</h2>
-        <div className="form-control">
-          <label htmlFor="username">Username</label>
-          <input
-             type="text"
-             name="username"
-             value={formData.username}
-             onChange={handleChange}
-          />
-        </div>
-        <div className="form-control">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-control">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-control">
-          <label htmlFor="repeat-password">Repeat Password</label>
-          <input
-            type="password"
-            name="repeatPassword"
-            value={formData.repeatPassword}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button className="submit-button" type="submit">
-          Sign Up
-        </button>
-        <p className="account-info">
-          Already have an account?
-          <Link href="/fantasy/Login" className="login-link">
-            Log In instead
-          </Link>
-        </p>
-      </form>
+      <Container fluid>
+        <Row>
+          <Col xs={8}>
+            <Image
+              className="img-signup"
+              fluid="true"
+              rounded
+              src="https://images.unsplash.com/photo-1510051640316-cee39563ddab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt="Premier League"
+            />
+          </Col>
+          <Col>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Enter Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                  placeholder="Enter email"
+                  required
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Enter email"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Password"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Repeat Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Repeat Password"
+                  required
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+              <p className="account-info">
+                Already have an account?
+                <Link href="/fantasy/Login" className="login-link">
+                  Log In instead
+                </Link>
+              </p>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
