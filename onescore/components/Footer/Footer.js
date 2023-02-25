cff21cec483a22faa49360805f33d4533e01c423
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-02-25 20:55:14 +0545

    header and footer sorted

--- onescore/components/Footer/Footer.js
+++ onescore/components/Footer/Footer.js
@@ -1,21 +1,54 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LanguageIcon from "@mui/icons-material/Language";
 
 function Footer() {
   return (
-    <div className='footer' >
-        <p>&#169; OneScore 2023</p>
-        <Link href='https://github.com/nabinkatwal7/fantasyfootball' >
-          <img src='https://img.icons8.com/ios-glyphs/512/github.png' />
-        </Link>
-        <Link href='https://twitter.com/campistaaaaa' >
-          <img src='https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png' />
-        </Link>
-        <Link href='https://nabinkatwal7.github.io/portfolio/?fbclid=IwAR0eL6yqyQiOVbkD8nfVXvRC-aTP6OXNXjLjGaTkiWsSU7tCEgdpyX-f6IA' >
-          <img src='https://www.freepnglogos.com/uploads/logo-website-png/logo-website-web-logo-clip-art-clkerm-vector-clip-art-online-11.png' />
-        </Link>
+    <div>
+      <footer
+        className="text-center text-white"
+        style={{ "background-color": "#f1f1f1" }}
+      >
+        <div className="container pt-4">
+          <section className="mb-4">
+            <Link
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              role="button"
+              data-mdb-ripple-color="dark"
+              href="https://github.com/nabinkatwal7/fantasyfootball"
+            >
+              <GitHubIcon />
+            </Link>
+            <Link
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              role="button"
+              data-mdb-ripple-color="dark"
+              href="https://twitter.com/campistaaaaa"
+            >
+              <TwitterIcon />
+            </Link>
+            <Link
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              role="button"
+              data-mdb-ripple-color="dark"
+              href="https://nabinkatwal7.github.io/portfolio/?fbclid=IwAR0eL6yqyQiOVbkD8nfVXvRC-aTP6OXNXjLjGaTkiWsSU7tCEgdpyX-f6IA"
+            >
+              <LanguageIcon />
+            </Link>
+          </section>
+        </div>
+        <div
+          className="text-center text-dark p-3"
+          style={{ "background-color": "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2023 Copyright:Nabin Katwal
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- onescore/components/Navbar/Navbar.js
+++ onescore/components/Navbar/Navbar.js
@@ -1,27 +1,28 @@
 import Link from "next/link";
 import React from "react";
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navba from 'react-bootstrap/Navbar';
+
 import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
 import NfcIcon from "@mui/icons-material/Nfc";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <ul>
-        <li className="onescore-logo" >OneScore</li>
-        <li>
-          <Link href="/">
+    <div>
+      <Navba fixed="top" bg="primary" variant="dark">
+        <Container>
+          <Navba.Brand href="/">
             <SportsSoccerIcon />
-            Live Score
-          </Link>
-        </li>
-        <li>
-          <Link href="/fantasy">
-            <NfcIcon />
-            Fantasy
-          </Link>
-        </li>
-      </ul>
+            OneScore
+          </Navba.Brand>
+          <Nav className="me-auto navbar navbar-dark bg-primary">
+            <Nav.Link href="/">Live Score</Nav.Link>
+            <Nav.Link href="/fantasy">Fantasy</Nav.Link>
+          </Nav>
+        </Container>
+      </Navba>
     </div>
   );
 }
